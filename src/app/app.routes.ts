5e89add63e9b39b939957cab288c209b3408c4e1
Mateo Rodriguez <mateor861@gmail.com>
2025-05-22 16:08:45 -0300

    Feat: ??

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -7,7 +7,19 @@ export const routes: Routes = [
   },
   {
     path: '',
-    redirectTo: 'home',
+    redirectTo: 'login',
     pathMatch: 'full',
+  },
+  {
+    path: 'login',
+    loadComponent: () => import('./pages/login/login.page').then( m => m.LoginPage)
+  },
+  {
+    path: 'register',
+    loadComponent: () => import('./pages/register/register.page').then( m => m.RegisterPage)
+  },
+  {
+    path: 'jornada-board',
+    loadComponent: () => import('./pages/jornada-board/jornada-board.page').then( m => m.JornadaBoardPage)
   },
 ];
